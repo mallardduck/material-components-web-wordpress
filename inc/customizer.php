c90b56bcdf3c81314bcc8d693fe209eed3c02cfc
Dan Pock <dpock32509@gmail.com>
2018-08-09 17:42:17 -0400

    set fallback for when the theme option isn't set
    this sets a fallback for when the theme customizer hasn't been used yet. in these cases the value returns false and then is echo'd into the variable as an empty string. In some cases the end result makes the CSS variable set to "" which turns into a transparent background.

--- inc/customizer.php
+++ inc/customizer.php
@@ -3,34 +3,34 @@
 function mdcwp_customize_register( $wp_customize ) {
 	$wp_customize->add_section('textcolors', array('title'=>'Theme colors'));
 	$txtcolors[] = array(
-    'slug'=>'primary_color_custom', 
+    'slug'=>'primary_color_custom',
     'default' => '#3f51b5',
     'label' => 'Primary Color'
 	);
- 
+
 	// secondary color ( site description, sidebar headings, h3, h5, nav links on hover )
 	$txtcolors[] = array(
-    'slug'=>'accent_color_custom', 
+    'slug'=>'accent_color_custom',
     'default' => '#ff4081',
     'label' => 'Accent Color'
 	);
-	
+
 	foreach( $txtcolors as $txtcolor ) {
- 
+
     // SETTINGS
     $wp_customize->add_setting(
         $txtcolor['slug'], array(
             'default' => $txtcolor['default'],
-            'type' => 'option', 
+            'type' => 'option',
             'capability' =>  'edit_theme_options'
 			)
 		);
-    
+
 	$wp_customize->add_control(
     new WP_Customize_Color_Control(
         $wp_customize,
-        $txtcolor['slug'], 
-        array('label' => $txtcolor['label'], 
+        $txtcolor['slug'],
+        array('label' => $txtcolor['label'],
         'section' => 'textcolors',
         'settings' => $txtcolor['slug'])
 		)
@@ -40,9 +40,8 @@ function mdcwp_customize_register( $wp_customize ) {
 add_action('customize_register', 'mdcwp_customize_register');
 
 function mdcwp_customize_colors() {
-	$primary_color_custom = get_option( 'primary_color_custom' );
-
-	$accent_color_custom = get_option( 'accent_color_custom' );
+	$primary_color_custom = get_option( 'primary_color_custom' ) ?: 'unset';
+	$accent_color_custom = get_option( 'accent_color_custom' ) ?: 'unset';
 	?>
 	<style>
 	:root {
